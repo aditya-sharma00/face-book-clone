3cd205bbb13e6ec4c9353f682676854df1e53e41
Aditya Sharma <adityawb902@gmail.com>
2022-12-12 21:40:14 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React,{Component} from "react";
 import './App.css'
-import img from './image/fb2.jpeg'
+import img from './image/fb.png'
 import axios from 'axios'
 class App extends Component{
     constructor(){
